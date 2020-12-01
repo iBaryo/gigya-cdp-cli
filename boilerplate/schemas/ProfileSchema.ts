a365dc3499ded1ad6c5b4189fb6394ecfbe22735
Zoe Cohen <zoe.cohen@sap.com>
2020-12-01 16:48:22 +0200

    profileSchema code-review

--- boilerplate/schemas/ProfileSchema.ts
+++ boilerplate/schemas/ProfileSchema.ts
@@ -2,11 +2,14 @@ import {JSONSchema7} from "json-schema";
 import {ProfileFieldName} from "../../gigya-cdp-sdk/entities";
 
 export const profileSchema: JSONSchema7 = {
-    "firstName": {type: "string"},
-    "primaryEmail": {type: "string"},
-    "primaryPhone": {type: "string"},
-    "masterDataId": {type: ["string"]},
-    "gender": {type: "string"},
-    "birthdate": {type: "string", format: "date"}
-    // "age": {type: "number"}
-} as JSONSchema7;
+    type: 'object',
+    properties: {
+        "firstName": {type: "string"},
+        "primaryEmail": {type: "string"},
+        "primaryPhone": {type: "string"},
+        "masterDataId": {type: ["string"]},
+        "gender": {type: "string"},
+        "birthdate": {type: "string", format: "date"}
+        // "age": {type: "number"}
+    }
+};
