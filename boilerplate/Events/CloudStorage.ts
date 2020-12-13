8ac66aacc0e8e1eebe5719c69b94c9895267c535
Zoe Cohen <zoe.cohen@sap.com>
2020-12-13 13:05:16 +0200

    added alignMatchRules() after sligning profile schema

--- boilerplate/Events/CloudStorage.ts
+++ boilerplate/Events/CloudStorage.ts
@@ -1,17 +1,17 @@
-// import {Payload} from "../../gigya-cdp-sdk/entities/common";
-// import {Event} from "../../gigya-cdp-sdk/entities/Event";
-// import {config} from "../BoilerplateConfig";
-//
-// export const cloudStorageEventTemplate: Payload<Event> = {
-//     enabled: false,
-//     schema: {
-//         type: 'object',
-//         properties: {
-//             [config.commonIdentifier]: {
-//                 type: 'string'
-//             },
-//             // ...
-//         }
-//     }
-//
-// }; // TODO: zoe
+import {Payload} from "../../gigya-cdp-sdk/entities/common";
+import {Event} from "../../gigya-cdp-sdk/entities/Event";
+import {config} from "../BoilerplateConfig";
+
+export const cloudStorageEventTemplate: Payload<Event> = {
+    dataType: undefined, name: "", purposeIds: [],
+    enabled: false,
+    schema: {
+        type: 'object',
+        properties: {
+            [config.commonIdentifier]: {
+                type: 'string'
+            },
+        }
+    }
+
+}; // TODO: zoe
---
+++ boilerplate/MatchRules/masterDataId.ts
@@ -0,0 +1,6 @@
+import {DirectEventName} from "../Events/Direct";
+import {Payload} from "../../gigya-cdp-sdk/entities/common";
+import {Schema} from "json-schema-faker";
+import {ProfileSchemaName} from "../../gigya-cdp-sdk/entities";
+
+// export const commonIdentifier: Payload<> = {}
