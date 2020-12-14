24389779ca57d12d3ff979ef3b20b0dcc27289d2
Zoe Cohen <zoe.cohen@sap.com>
2020-12-14 15:17:23 +0200

    matchRules changes and cloudStorage Applications are created if they don't exist

--- boilerplate/Events/CloudStorage.ts
+++ boilerplate/Events/CloudStorage.ts
@@ -1,17 +1,15 @@
 import {Payload} from "../../gigya-cdp-sdk/entities/common";
 import {Event} from "../../gigya-cdp-sdk/entities/Event";
 import {config} from "../BoilerplateConfig";
+import {PurposeName} from "../purposes/purposes";
 
 export const cloudStorageEventTemplate: Payload<Event> = {
-    dataType: undefined, name: "", purposeIds: [],
-    enabled: false,
-    schema: {
-        type: 'object',
-        properties: {
-            [config.commonIdentifier]: {
-                type: 'string'
-            },
-        }
-    }
+    configValues: undefined,
+    dataType: undefined,
+    description: "",
+    enabled: true,
+    name: "",
+    purposeIds: ["basic", "marketing"] as Array<PurposeName>,
+    schema: undefined
+}
 
-}; // TODO: zoe
