3fd346e44687b812d245864aeac610f93225a6a7
Zoe Cohen <zoe.cohen@sap.com>
2020-12-30 15:43:18 +0200

    few changes for Basic --> Direct

--- boilerplate/Applications/defaultDirectApplication.ts
+++ boilerplate/Applications/defaultDirectApplication.ts
@@ -4,7 +4,7 @@ import {Purpose} from "../BoilerplateConfig";
 import {Application} from "../../gigya-cdp-sdk/entities/Application";
 
 export const defaultDirectApplication: Payload<Application> = {
-    type: 'Basic',
+    type: 'Direct',
     enabled: true,
     logoUrl: "https://universe.eu5-st1.gigya.com/assets/img/connect-application.png",
     name: "Direct Test Application",
